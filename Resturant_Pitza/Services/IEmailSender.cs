bc4dcf0629b974a58339abb22daf5eb1b4c5a4fe
samer <samer_mail_2006@yahoo.com>
2017-04-08 19:33:53 +0300

    Add project files.

---
+++ Resturant_Pitza/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resturant_Pitza.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
